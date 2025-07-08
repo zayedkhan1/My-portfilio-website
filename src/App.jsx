c8944f3c2baf730e733fec23df347a1f897b533b
zayed khan <zayedk458@gmail.com>
2025-07-08 23:33:31 +0600

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
 
   return (
     <div className='bg-[#050414]'>
-      <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }}></BlurBlob>
+      <BlurBlob position={{ top: '35%', left: '20%' }}  size={{ width: '30%', height: '40%' }}></BlurBlob>
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
       </div>
-      <div className="relative pt-20">
+      <div className="relative pt-20 ">
         <NavBar></NavBar>
         <About></About>
         <Skills></Skills>
